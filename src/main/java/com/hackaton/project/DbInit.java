dface4a039b905b52c96733b72c1f8fc5483b8ca
Tumilok <vladislav.tumilovich@gmail.com>
2020-08-29 20:51:15 +0200

    Implemented authentication

--- src/main/java/com/hackaton/project/DbInit.java
+++ src/main/java/com/hackaton/project/DbInit.java
@@ -1,25 +1,32 @@
 package com.hackaton.project;
 
+import com.hackaton.project.models.ERole;
 import com.hackaton.project.models.Project;
+import com.hackaton.project.models.Role;
 import com.hackaton.project.models.User;
 import com.hackaton.project.repository.ProjectRepository;
+import com.hackaton.project.repository.RoleRepository;
 import com.hackaton.project.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class DbInit implements CommandLineRunner {
     private UserRepository userRepository;
     private ProjectRepository projectRepository;
+    private RoleRepository roleRepository;
 
-    public DbInit(UserRepository userRepository, ProjectRepository projectRepository) {
+    public DbInit(UserRepository userRepository, ProjectRepository projectRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.projectRepository = projectRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User("alrkg", "sdgj", "eorgj@gmail.com", "234234");
+        User user = new User("nickname1", "eorgj@gmail.com", "234234");
         userRepository.save(user);
 
         Project project = new Project("Some title");
@@ -30,5 +37,9 @@ public class DbInit implements CommandLineRunner {
 
         userRepository.save(user);
         projectRepository.save(project);
+
+        Role roleUser = new Role(ERole.ROLE_USER);
+        Role roleMod = new Role(ERole.ROLE_MODERATOR);
+        roleRepository.saveAll(Arrays.asList(roleMod, roleUser));
     }
 }
---
+++ src/main/java/com/hackaton/project/models/Role.java
@@ -0,0 +1,51 @@
+package com.hackaton.project.models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
