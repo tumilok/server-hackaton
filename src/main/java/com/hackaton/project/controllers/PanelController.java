40758a49d8aa74f10bcf809110fd12c068c9e3a1
Tumilok <vladislav.tumilovich@gmail.com>
2020-08-30 02:19:46 +0200

    Implemented few api's

--- src/main/java/com/hackaton/project/controllers/PanelController.java
+++ src/main/java/com/hackaton/project/controllers/PanelController.java
@@ -2,18 +2,19 @@ package com.hackaton.project.controllers;
 
 import com.hackaton.project.models.Project;
 import com.hackaton.project.models.Task;
+import com.hackaton.project.models.User;
 import com.hackaton.project.repository.ProjectRepository;
 import com.hackaton.project.repository.TaskRepository;
+import com.hackaton.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
 @RequestMapping("api/panel")
 public class PanelController {
     @Autowired
@@ -22,16 +23,30 @@ public class PanelController {
     @Autowired
     ProjectRepository projectRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/tasks/{projectId}")
-    public List<Task> projectTasks(@PathVariable Long projectId) {
-        Optional<Project> project = projectRepository.findById(projectId);
-        if(project.isPresent()) {
-            return taskRepository.findByProject(project.get());
-        }
-        else {
-            return null;
-        }
+    public List<Task> projectTasks(@PathVariable String projectId) {
+        Optional<Project> project = projectRepository.findById(Long.parseLong(projectId));
+        return project.map(value -> taskRepository.findByProject(value)).orElse(null);
     }
 
-
+    @GetMapping("/projects/{userId}")
+    public List<Project> getUserProjects(@PathVariable Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            List<Project> projects = projectRepository.findAll();
+            List<Project> returnList = new ArrayList<>();
+            for (Project p: projects) {
+                if (p.getUsers().contains(user.get())) {
+                    returnList.add(p);
+                }
+            }
+            if (returnList.size() > 0) {
+                return returnList;
+            }
+        }
+        return null;
+    }
 }
